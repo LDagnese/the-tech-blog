25551e3582862d7b10165341645fac2a2e8528c9
loudagnese <louis.dagnese@gmail.com>
2022-08-19 19:48:52 -0700

    Comment model done and routes done

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,11 +1,19 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Comment } = require("../../models");
 
 // GET all Posts
 router.get("/", (req, res) => {
   Post.findAll({
     attributes: ["id", "title", "content", "created_at"],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "content", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -27,6 +35,14 @@ router.get("/:id", (req, res) => {
     },
     attributes: ["id", "title", "content", "created_at"],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "content", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
