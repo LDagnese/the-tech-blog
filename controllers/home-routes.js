4a9933122b8fcf5fb996d1aa64e4735feb7f5de6
loudagnese <louis.dagnese@gmail.com>
2022-08-20 11:05:46 -0700

    homepage view works

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
+const { Post, User } = require("../models");
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  Post.findAll({
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      console.log(posts);
+      res.render("homepage", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
