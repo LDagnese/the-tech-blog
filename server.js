2231dbee4264c29178a481933988f10231368bab
loudagnese <louis.dagnese@gmail.com>
2022-08-20 08:06:43 -0700

    add header and footer to main.handlebars, apply css class and background color

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const path = require("path");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+// set the template engine to Handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
