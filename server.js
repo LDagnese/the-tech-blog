dbdbf402b46168469a98f3263963547ea523575b
loudagnese <louis.dagnese@gmail.com>
2022-08-22 18:35:33 -0700

    add session ability

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.SESS_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ db: sequelize }),
+};
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -12,7 +23,7 @@ const PORT = process.env.PORT || 3002;
 // set the template engine to Handlebars
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
