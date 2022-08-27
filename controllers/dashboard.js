f09bf1e70666c87edf7ac5ce784a1fcecef25456
loudagnese <louis.dagnese@gmail.com>
2022-08-27 11:43:41 -0700

    added partials to comments and posts

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -25,7 +25,6 @@ router.get("/", withAuth, (req, res) => {
   })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      console.log(posts.length);
       res.render("dashboard", { posts, loggedIn: true });
     })
     .catch((err) => {
